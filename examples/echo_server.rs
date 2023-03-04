920bfd9dc7af707be6d4cd06e9b974ce0c5d6b33
messense <messense@icloud.com>
2023-03-04 22:48:45 +0800

    Add echo_server example

---
+++ examples/echo_server.rs
@@ -0,0 +1,30 @@
+//! To build and run it:
+//! 
+//! ```bash
+//! TS_AUTHKEY=<your-auth-key> cargo run --example echo_server
+//! ```
+use std::io::{Read, Write};
+
+use libtailscale::Tailscale;
+
+fn main() {
+    let mut ts = Tailscale::new();
+    ts.set_ephemeral(true).unwrap();
+    ts.up().unwrap();
+
+    let mut listener = ts.listen("tcp", ":1999").unwrap();
+    loop {
+        let mut stream = listener.accept().unwrap();
+        let mut buf = [0; 2048];
+        while match stream.read(&mut buf) {
+            Ok(size) => {
+                stream.write_all(&buf[..size]).unwrap();
+                true
+            }
+            Err(_) => {
+                stream.shutdown(std::net::Shutdown::Both).unwrap();
+                false
+            }
+        } {}
+    }
+}
